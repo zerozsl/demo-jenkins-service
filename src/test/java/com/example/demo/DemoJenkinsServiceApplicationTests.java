db104e4151197b558e63465591fe3c414b25588e
zengjinlong <zengjinlong@xunlei.com>
2019-11-25 14:12:16 +0800

    java

---
+++ src/test/java/com/example/demo/DemoJenkinsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
